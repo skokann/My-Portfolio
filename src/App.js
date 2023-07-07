6b73f80d0c52139778a65b20c28d18693c52fb70
Jiří Zubryckyj <64204363+skokann@users.noreply.github.com>
2023-07-07 15:23:05 +0200

    started project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='w-full h-full min-h-screen bg-black'>
+      <h1 className='text-white text-9xl'>JIŘÍ   ZUBRYCKYJ</h1>
+      </div>
+      <div className='w-full h-full min-h-screen bg-black'>
+
+      </div>
+      <div className='w-full h-full min-h-screen bg-black'>
+
+      </div>
+      <div className='w-full h-full min-h-screen bg-black'>
+
+      </div>
+      <div className='w-full h-full min-h-screen bg-black'>
+
+      </div>
     </div>
   );
 }
