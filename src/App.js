f83c03081ad07fefef70d38c3a8689adffedb9d4
Jiří Zubryckyj <64204363+skokann@users.noreply.github.com>
2023-07-07 15:25:12 +0200

    started a new project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div className='w-full h-full min-h-screen bg-black'>
-      <h1 className='text-white text-9xl'>JIŘÍ   ZUBRYCKYJ</h1>
+      <h1 className='text-white text-9xl'>JIŘÍ ZUBRYCKYJ</h1>
       </div>
       <div className='w-full h-full min-h-screen bg-black'>
 
