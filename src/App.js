4290852123e8feff006597fa307667552280bfa2
Jiří Zubryckyj <64204363+skokann@users.noreply.github.com>
2023-07-09 12:02:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className='w-full h-full min-h-screen px-20 bg-black
-      '>
-      <h1 className='text-orange-300 md:text-3xl lg:text-6xl xl:text-9xl text-left'>JIŘÍ <br></br> ZUBRYCKYJ</h1>
+      <div className='w-full h-full min-h-screen bg-black px-16 '>
+        <div className='flex flex-col' >
+      <h1 className='text-white text-2xl '>JIŘÍ <br></br> ZUBRYCKYJ</h1>
+      <p className='text-white text-xl text-left'>Junior freelance developer skilled in React, HTML, CSS, and JavaScript. Committed to <br></br>  delivering high-quality projects with efficiency and creativity.</p>
+      <button class="bg-emerald-500 hover:bg-emerald-400 text-white font-bold py-2 px-4 rounded-lg">
+  Button
+</button>
+</div>
+      <div className='w-full h-full min-h-screen bg-black'>
+      <p className='text-white text-xl'>Henlo</p>
       </div>
       <div className='w-full h-full min-h-screen bg-black'>
 
@@ -16,9 +23,7 @@ function App() {
       <div className='w-full h-full min-h-screen bg-black'>
 
       </div>
-      <div className='w-full h-full min-h-screen bg-black'>
-
-      </div>
+    </div>
     </div>
   );
 }
